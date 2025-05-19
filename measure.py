4be811e077a00172db6df975b9ab73b8443442d0
zoldof <zoldof.game@gmail.com>
2025-05-19 15:57:03 +0900

    Update measure.py

--- measure.py
+++ measure.py
@@ -1,14 +1,14 @@
-def measure_all(func, args):
+def measure(args):
     tracemalloc.start()
     start_time = time.perf_counter()
 
-    result = func(args) 
+    result = main(args) 
 
     end_time = time.perf_counter()
     current, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
-
     elapsed_time = end_time - start_time
+    
     result_data = {
         "実行時間　　　　": f"{elapsed_time:.6f} 秒",
         "メモリ（現時点）": f"{current} bytes",
@@ -18,6 +18,3 @@ def measure_all(func, args):
 
     json_str = json.dumps(result_data, ensure_ascii=False)
     return json_str
-
-def measure(name):  
-    return measure_all(main, name)
