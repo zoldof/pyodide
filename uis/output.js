90dae8ecd285ef332e7e95629f5f7f796ff31048
zoldof <zoldof.game@gmail.com>
2025-05-19 13:33:02 +0900

    Update output.js

--- uis/output.js
+++ uis/output.js
@@ -15,7 +15,7 @@ function formatResult(resultData) {
 }
 
 export function showOutput(result) {
-  //const data = JSON.parse((result);
+  const data = JSON.parse((result);
   //const output = formatResult(date)
-  document.getElementById("output").textContent = result;
+  document.getElementById("output").textContent = date;
 }
