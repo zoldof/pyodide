f343a7106399c8d26f0ae5ccba54ddc7c9551073
zoldof <zoldof.game@gmail.com>
2025-05-19 16:41:46 +0900

    Update hello_input.js

--- uis/hello_input.js
+++ uis/hello_input.js
@@ -1,3 +1,17 @@
-export function inputUI() {
-  return prompt("あなたの名前は？") || "名無しの旅人";
+// ← import はファイルの先頭
+import Swal from 'sweetalert2';
+
+// 名前入力UIのエクスポート関数
+export async function inputUI() {
+  const config = {
+    title: "名前の入力",
+    input: "text",
+    inputPlaceholder: "あなたの名前は？",
+    showCancelButton: true,
+    confirmButtonText: "決定",
+    cancelButtonText: "キャンセル"
+  };
+
+  const { value, isConfirmed } = await Swal.fire(config);
+  return (isConfirmed && value) || "名無しの旅人";
 }
