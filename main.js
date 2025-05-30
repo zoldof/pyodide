ce054a66c6730088907e94e23190203a7ec568af
zoldof <zoldof.game@gmail.com>
2025-05-30 12:13:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -23,8 +23,12 @@ const inputModule = await import(`./uis/${ui}_input.js`).catch(() => {});
 if (inputModule?.inputUI) {
   inputUI = inputModule.inputUI;
 }
+
+const output = document.getElementById("output");
+output.textContent = "pyodide読み込み前";
 // pyodideでpyの関数を使用
 const result = pyFunc(...await inputUI());
+output.textContent = result;
 pyFunc.destroy();
 
 // 出力UI（必ず用意されている前提）
