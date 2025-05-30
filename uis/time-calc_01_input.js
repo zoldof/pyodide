d15aa85b62ed3277468eece8e328d3fa08b4e0d2
zoldof <zoldof.game@gmail.com>
2025-05-30 14:16:26 +0900

    Update time-calc_01_input.js

--- uis/time-calc_01_input.js
+++ uis/time-calc_01_input.js
@@ -5,10 +5,8 @@ export function inputUI() {
 
   while (true) {
     try {
-      startStr = prompt("開始時刻を入力してください（hh:mm:ss）");
-      parseTimeInput(startStr);
-      endStr   = prompt("終了時刻を入力してください（hh:mm:ss）");
-      parseTimeInput(endStr);
+      startStr = parseTimeInput(prompt("開始時刻を入力してください（hh:mm:ss）"));
+      endStr   = parseTimeInput(prompt("終了時刻を入力してください（hh:mm:ss）"));
       validateStartEnd(startStr, endStr);
       break;
     } catch (e) {
