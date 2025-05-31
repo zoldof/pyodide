99d8a2110c52d4ec3f42f24e25774613d2d870c9
zoldof <zoldof.game@gmail.com>
2025-05-31 09:51:15 +0900

    Update time-calc_01_input.js

--- uis/time-calc_01_input.js
+++ uis/time-calc_01_input.js
@@ -5,17 +5,26 @@ function delay(ms) {
 }
 
 async function getTimeInputLoop(message) {
+  const config = {
+    title: "時刻の入力",
+    input: "text",
+    inputPlaceholder: message,
+    showCancelButton: true,
+    confirmButtonText: "決定",
+    cancelButtonText: "キャンセル"
+  };
+  
   while (true) {
-    const input = prompt(message);
+    const { value, isConfirmed } = await Swal.fire(config);
 
-    if (input === null) {
+    if (!(isConfirmed && value)) {
       alert("キャンセルされました。5秒後にもう一度お聞きします。");
       await delay(5000); // 5秒待つ
       continue; // もう一度 prompt に戻る
     }
 
     try {
-      return parseTimeInput(input);
+      return parseTimeInput(value);
     } catch (e) {
       alert(e.message);
     }
